017c21b3098540b1bf3c74a8485da99ba6aab5c5
Jose Ant. Beltrán <jbeltrma@everis.com>
2020-11-17 00:23:06 +0100

    Material mñn

---
+++ CreditRatingClient/Program.cs
@@ -0,0 +1,40 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+using GreeterService;
+using CreditRatingService;
+
+namespace CreditRatingClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            var client1 = new Greeter.GreeterClient(channel);
+            var greetingRequest = new HelloRequest { Name = "Beltrán" };            
+            var reply1 = await client1.SayHelloRequestAsync(greetingRequest);
+            Console.WriteLine(reply1.Message);
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+
+
+            var client = new CreditRatingCheck.CreditRatingCheckClient(channel);
+            var creditRequest = new CreditRequest { CustomerId = 201, CreditQuantity = 7000 };
+            var reply = await client.CheckCreditRequestAsync(creditRequest);
+
+            Console.WriteLine($"El credito para el cliente con Id {creditRequest.CustomerId} esta {(reply.IsAccepted ? "aprobado" : "rechazado")}!");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+
+            creditRequest = new CreditRequest { CustomerId = 201, CreditQuantity = 40000 };
+            reply = await client.CheckCreditRequestAsync(creditRequest);
+
+            Console.WriteLine($"El credito para el cliente con Id {creditRequest.CustomerId} esta {(reply.IsAccepted ? "aprobado" : "rechazado")}!");
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GrpcServer/Services/CreditRatingService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CreditRatingService;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace GrpcServer.Services
+{
+    public class CreditRatingCheckService : CreditRatingCheck.CreditRatingCheckBase
+    {
+        private readonly ILogger<CreditRatingCheckService> _logger;
+        private static readonly Dictionary<Int32, Int32> customerTrustedCredit = new Dictionary<Int32, Int32>()
+        {
+            {201, 10000},
+            {417, 5000},
+            {306, 15000}
+        };
+        public CreditRatingCheckService(ILogger<CreditRatingCheckService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CreditReply> CheckCreditRequest(CreditRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new CreditReply
+            {
+                IsAccepted = IsEligibleForCredit(request.CustomerId, request.CreditQuantity)
+            });
+        }
+
+        private bool IsEligibleForCredit(Int32 customerId, Int32 credit)
+        {
+            bool isEligible = false;
+
+            if (customerTrustedCredit.TryGetValue(customerId, out Int32 maxCredit))
+            {
+                isEligible = credit <= maxCredit;
+            }
+
+            return isEligible;
+        }
+    }
+}
